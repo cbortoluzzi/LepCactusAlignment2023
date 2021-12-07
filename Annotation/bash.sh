facfc5dc92a43164ad2fdc5ebf4ab2a2bb5781f3
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2021-12-07 14:00:12 +0000

    Create bash.sh

---
+++ Annotation/bash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+# Author : @cb46
+
+echo "python3 get_ensembl_annotation.py -table vertebrata_202111.tsv -csv Species_202112.csv -metadata species_metadata.json"
+python3 get_ensembl_annotation.py -table vertebrata_202111.tsv -csv Species_202112.csv -metadata species_metadata.json
