d066b58f6252424533af14d9e268ee6a880b8008
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2021-12-07 13:27:26 +0000

    Update bash.sh

--- DeepFin_v4/bash.sh
+++ DeepFin_v4/bash.sh
@@ -4,7 +4,7 @@
 # Author : @cb46
 
 
-echo "python3 order_by_DeepFin_v4.py DeepFin_v4.csv vertebrata_202111.tsv"
-python3 order_by_DeepFin_v4.py DeepFin_v4.csv vertebrata_202111.tsv  
+echo "python3 order_by_DeepFin_v4.py DeepFin_v4.csv fish_202111.tsv"
+python3 order_by_DeepFin_v4.py DeepFin_v4.csv fish_202111.tsv  
 
 
