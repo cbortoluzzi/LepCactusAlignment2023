c280da84ea3b4fc28343138db67a26d3bc88669a
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2021-12-07 09:40:58 +0000

    Create bash.sh

---
+++ DeepFin_v4/bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# Author : @cb46
+
+
+echo "python3 order_by_DeepFin_v4.py DeepFin_v4.csv vertebrata_202111.tsv"
+python3 order_by_DeepFin_v4.py DeepFin_v4.csv vertebrata_202111.tsv  
