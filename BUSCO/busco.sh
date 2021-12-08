96d00f64a41b9508e2aa5f7c07c3ecabee5a3ab1
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2021-12-08 12:08:28 +0000

    Update busco.sh

--- BUSCO/busco.sh
+++ BUSCO/busco.sh
@@ -16,19 +16,13 @@ fi
 
 FASTA=$1
 LINEAGE=$2
-
-
-SPECIES=`echo $FASTA | rev | cut -d"/" -f2 | rev`
 DIR=$(dirname $FASTA)
 
 
 echo "./busco.sh $FASTA $LINEAGE"
 
 
-echo "busco -i $FASTA -l $LINEAGE -o $SPECIES -c 8 -m genome --offline"
-busco -i $FASTA -l $LINEAGE -o $SPECIES -c 8 -m genome --offline
+echo "busco -i $FASTA -l $LINEAGE -o $DIR/vertebrata_odb10_metaeuk -c 8 -m genome --offline"
+busco -i $FASTA -l $LINEAGE -o $DIR/vertebrata_odb10_metaeuk -c 8 -m genome --offline
 
-
-mv $SPECIES vertebrata_odb10_metaeuk
-mv vertebrata_odb10_metaeuk $DIR
 
