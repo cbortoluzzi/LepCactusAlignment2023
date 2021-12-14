871ee919a25c0d65fd86d6ae032f0a1b029f752d
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2021-12-14 13:25:19 +0000

    Update bash.sh

--- Annotation/bash.sh
+++ Annotation/bash.sh
@@ -4,4 +4,4 @@
 # Author : @cb46
 
 
-python3 get_ensembl_annotation.py -table vertebrata_202111.tsv -csv Species_202112.csv -metadata species_metadata.json
+python3 get_ensembl_genome_annotation.py -table -metadata -o 
