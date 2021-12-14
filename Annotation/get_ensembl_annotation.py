ac1c19919854cfe2e812a2db6439c1e5ebc7db97
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2021-12-14 13:18:05 +0000

    Update get_ensembl_annotation.py

--- Annotation/get_ensembl_annotation.py
+++ Annotation/get_ensembl_annotation.py
@@ -1,75 +1,75 @@
 #!/usr/bin/env python
 
 
-# Author: @cb46
+# Author : @cb46
 
 
 import os
-import csv
 import json
 import argparse
 from pathlib import Path
 
 
-
-parser = argparse.ArgumentParser(description = 'Use Ensembl rapid release metadata to download annotations')
-parser.add_argument('-table', help = 'Vertebrata table v.2021-11')
-parser.add_argument('-csv', help = 'Ensembl rapid release species list')
+parser = argparse.ArgumentParser(description = 'Download assemblies from Ensembl Rapid Release')
+parser.add_argument('-table', help = 'Ensembl table v.2021-11')
 parser.add_argument('-metadata', help = 'Ensembl rapid release metadata')
+parser.add_argument('-o', help = 'File with output directories, one per line')
 
 
 
-path = {'Fish' : '/lustre/scratch123/tol/teams/durbin/users/cb46/vertebrate_genomes_project/pan_vertebrates_alignment/fish/busco.v5',
-	'Birds' : '/lustre/scratch123/tol/teams/durbin/users/cb46/vertebrate_genomes_project/pan_vertebrates_alignment/birds/busco.v5',
-	'Mammals' : '/lustre/scratch123/tol/teams/durbin/users/cb46/vertebrate_genomes_project/pan_vertebrates_alignment/mammals/busco.v5',
-	'Amphibians' : '/lustre/scratch123/tol/teams/durbin/users/cb46/vertebrate_genomes_project/pan_vertebrates_alignment/amphibians/busco.v5',
-	'Reptiles' : '/lustre/scratch123/tol/teams/durbin/users/cb46/vertebrate_genomes_project/pan_vertebrates_alignment/reptiles/busco.v5',
-	'Chordates' : '/lustre/scratch123/tol/teams/durbin/users/cb46/vertebrate_genomes_project/pan_vertebrates_alignment/chordates/busco.v5'
-	}
+def output_path(output):
+	mypath = {}
+	with open(output) as f:
+		for line in f:
+			taxon, path = line.strip().split()
+			mypath[taxon] = path
+	return mypath
 
 
 
-def assert_assembly(table, ensembl):
+def parse_table(table):
 	mydict = {}
-	annotated = {}
 	with open(table) as f:
 		next(f)
 		for line in f:
-			line = line.strip().split('\t')
-			assembly, assembly_name = line[0].split('.', 1)[1], line[1]
-			mydict[assembly] = assembly_name
-
-
-	with open(ensembl) as csv_f:
-		csv_r = csv.reader(csv_f, delimiter = ',')
-		next(csv_r)
-		for line in csv_r:
-			species_name, clade, assembly_name, assembly, annotation, rna_seq = line[0].replace(' ', '_'), line[3], line[5], line[6], line[9], line[11]
-			if assembly in mydict.keys() and assembly_name == mydict[assembly]:
-				annotated[assembly] = [species_name, assembly_name, clade, annotation, rna_seq]
-	return annotated
+			assembly_id, assembly_name, class_p, clade, order, family, species, size, contig_n50, scaffold_n50, assembly_level = line.strip().split('\t')
+			species_name = species.replace(' ', '_')
+			tol, assembly = assembly_id.split('.', 1)
+			mydict[assembly] = [species_name, assembly_name, clade]
+	return mydict
 
 
 
-def download_annotation(annotated, metadata, path):
+def download_genome_assembly(mydict, metadata, mypath):
 	with open(metadata) as json_f:
 		data = json.load(json_f)
 		for i in data:
 			assembly = str(i['assembly_accession'])
-			if assembly in annotated.keys():
-				geneset = str(i['geneset'])
-				ensembl_rapid_release = '/'.join(['ftp://ftp.ensembl.org/pub/rapid-release/species', annotated[assembly][0], assembly, 'geneset', geneset, '*'])
-				clade = annotated[assembly][2]
-				directory = Path(path[clade], annotated[assembly][0])
-				if Path(directory).is_dir():
-					cmd = 'wget -nc %s --directory-prefix=%s' % (ensembl_rapid_release, directory)
-					os.system(cmd)
+			geneset = str(i['geneset'])
+			if assembly in mydict.keys():
+				genome = mydict[assembly][0]+"-"+assembly+"-unmasked.fa.gz"
+				ensembl_genome = '/'.join(["ftp://ftp.ensembl.org/pub/rapid-release/species", mydict[assembly][0], assembly, "genome", genome])
+				ensembl_annotation = '/'.join(["ftp://ftp.ensembl.org/pub/rapid-release/species", mydict[assembly][0], assembly, "geneset", geneset, "*"])
+				class_p = mydict[assembly][2]
+				path = mypath[class_p]
+				directory = Path(path, mydict[assembly][0])
+				try:
+					directory.mkdir(parents=True, exist_ok=False)
+				except FileExistsError:
+					print ("Folder is already there")
+				cmd_genome = 'wget -nc %s --directory-prefix=%s' % (ensembl_genome, directory)
+				cmd_annotation = 'wget -nc %s --directory-prefix=%s' % (ensembl_annotation, directory)
+				print ("Downloading genome ....")
+				os.system(cmd_genome)
+				print ("Downloading annotation ...")
+				os.system(cmd_annotation)
 
 
 
 if __name__ == "__main__":
 	args = parser.parse_args()
-	check_assembly = assert_assembly(args.table, args.csv)
-	download = download_annotation(check_assembly, args.metadata, path)
-  
-  
+	output_f = output_path(args.o)
+	parse_file = parse_table(args.table)
+	download_assembly = download_genome_assembly(parse_file, args.metadata, output_f)
+
+	
