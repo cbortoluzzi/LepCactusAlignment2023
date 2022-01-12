174f84ad7c3ba9ef8b14c76f185f04210436406a
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-01-12 17:16:18 +0000

    Update _submit_prune_phylo.sh

--- Alignment/Guide_tree/_submit_prune_phylo.sh
+++ Alignment/Guide_tree/_submit_prune_phylo.sh
@@ -17,10 +17,11 @@ SPECIES_LIST=$2
 PRUNED_PHYLO=$3
 
 
+# Prune phylogenetic tree
 echo "python3 prune_phylo.py --tree $TREE --table $SPECIES_LIST --pruned $PRUNED_PHYLO"
 python3 prune_phylo.py --tree $TREE --table $SPECIES_LIST --pruned $PRUNED_PHYLO
 
-
+# Assign color to species based on group. This file can be used in iTOL: https://itol.embl.de/upload.cgi
 echo "python3 annotate_phylo.py --table $SPECIES_LIST"
 python3 annotate_phylo.py --table $SPECIES_LIST
 
