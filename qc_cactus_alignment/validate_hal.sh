553604ee627040b7fd0185d108807045d95527c5
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-02-15 14:30:56 +0000

    Create validate_hal.sh

---
+++ qc_cactus_alignment/validate_hal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+# Author: @cb46
+
+
+
+if [ -z $1 ]; then
+	echo "Usage: ./validate_hal.sh <cactus.alignment>"
+	exit -1
+fi
+
+
+HAL=$1
+
+# Check if the HAL database is valid; we can check this by simply running halValidate
+halValidate $HAL
