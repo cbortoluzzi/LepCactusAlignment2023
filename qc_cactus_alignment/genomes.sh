6a07147308a37a7209dc23fc7e7de56e6985e39f
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-05-16 16:35:19 +0100

    Create genomes.sh

---
+++ qc_cactus_alignment/genomes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+
+# Author: @cb46
+
+
+
+if [ -z $1 ]; then
+        echo "Usage: ./genomes.sh <input hal file>"
+        exit -1
+fi
+
+
+HAL=$1
+
+
+# Print list of species in the alignment
+halStats --genomes $HAL | tr -s ' '  '\n' | grep -v '^Anc' | sort > genomes_noAncestors.txt
