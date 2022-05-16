69e9a21a6f538921f5ff9c7bf3553c8746966764
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-05-16 16:48:48 +0100

    Update busco.sh

--- BUSCO/busco.sh
+++ BUSCO/busco.sh
@@ -9,21 +9,23 @@ conda activate /software/team118/miniconda3/envs/busco5
 
 
 if [[ -z $1 ]] ; then
-  echo "Usage: ./busco.sh <fasta> <busco.lineage>"
-  exit -1
+        echo "Usage: ./busco.sh <assembly in fasta format> <busco lineage>"
+        exit -1
 fi
 
 
 FASTA=$1
 LINEAGE=$2
-DIR=$(dirname $FASTA)
-SPECIES=`echo $FASTA | rev | cut -d"/" -f2 | rev`
 
 
-echo "./busco.sh $FASTA $LINEAGE"
+SPECIES=`echo $FASTA | rev | cut -d'/' -f2 | rev`
+OUTPUT=`echo $LINEAGE | rev | cut -d'/' -f2| rev`
+DIRECTORY=$(dirname $FASTA)
 
 
-echo "busco -i $FASTA -l $LINEAGE -o $SPECIES -c 8 -m genome --offline"
+# Run BUSCO using conda
 busco -i $FASTA -l $LINEAGE -o $SPECIES -c 8 -m genome --offline
 
+# Rename and move output
+mv $SPECIES $DIRECTORY/$OUTPUT
 
