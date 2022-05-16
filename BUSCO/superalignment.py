48eda3aba82635dbd2e496634926eb26aeaac9c3
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-05-16 16:50:57 +0100

    Update superalignment.py

--- BUSCO/superalignment.py
+++ BUSCO/superalignment.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 
 
-# Author: @cb46
-
-
 import argparse
+from sys import argv
 from pathlib import Path
 
 
 parser = argparse.ArgumentParser(description = 'Generate supermatrix for RAxML')
-parser.add_argument('-i', help = 'Name input directory')
-parser.add_argument('-o', help = 'Name output directory')
+parser.add_argument('--i', help = 'Name input directory')
+parser.add_argument('--o', help = 'Name output directory')
+
+
 
 
 
 def generate_supermatrix(in_folder, out_folder):
 	data = {}
 	key = None
-	files = list(Path(in_folder).rglob('*.trimal'))
+	files = list(Path(in_folder).rglob('*.trimAl'))
 	for f in files:
 		for line in open(f, 'r'):
 			if line.startswith('>'):
@@ -44,6 +44,4 @@ if __name__ == '__main__':
 	p.mkdir(parents=True, exist_ok=True)
 	supermatrix = generate_supermatrix(args.i, p)
 
-	
-	
 	
