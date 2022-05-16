4355c67fb07fcb107d00080c1415ae82a9b00f47
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-05-16 16:37:45 +0100

    Update coverage.sh

--- qc_cactus_alignment/coverage.sh
+++ qc_cactus_alignment/coverage.sh
@@ -5,35 +5,40 @@
 # Author: @cb46
 
 
+export PATH=/software/team118/maf_stream/target/release:$PATH
+
 
 if [ -z $1 ]; then
-	echo "Usage: ./coverage.sh <reference.genome> <cactus.alignment>"
-	exit -1
+        echo "Usage: ./coverage.sh <input hal file> <genome to calculate coverage on>"
+        exit -1
 fi
 
 
-REF=$1
-HAL=$2
+HAL=$1
+REF=$2
 
 
-mkdir -p coverage && mkdir -p coverage/$REF
+mkdir -p coverage/$refGenome
+
 
 # Obtain a tab delimited genome file that will look like this: <chromName><TAB><chromSize>
 halStats --bedSequences $REF $HAL | cut -f1,3 > coverage/$REF/$REF.genome
 
+
 # Let's now generate 100 random intervals, each 1,000,000 bp long
 bedtools random -g coverage/$REF/$REF.genome -l 1000000 -seed 12345 -n 100 > coverage/$REF/$REF.random.intervals
 
-# Let's now get an alignment for each of these 100 random intervals
+
+# Let's now get an alignment for each of these 1,000 random intervals
 cat coverage/$REF/$REF.random.intervals | while read contig start end num length strand
 do
-	bsub -R'select[mem>6000] rusage[mem=6000]' -M6000 -q basement -n 15 -G rdgroup -J coverage -o output_%J -e error_%J hal2maf --refSequence $contig --start $start --length `(expr $end - $start)` --refGenome $REF --onlyOrthologs --noAncestors $HAL coverage/$REF/$REF.random.intervals.$num.maf
+        echo "Generating an alignment for random interval number" $num
+        hal2maf --refSequence $contig --start $start --length `(expr $end - $start)` --refGenome $REF --onlyOrthologs --noAncestors $HAL coverage/$REF/$REF.random.intervals.$num.maf
+        # Calculate coverage
+        maf_stream coverage $REF coverage/$REF/$REF.random.intervals.$num.maf coverage/$REF/$REF.random.intervals.$num.maf.cov
 done
 
-# Calculate coverage using maf_stream
-for num in $(seq 1 100);do /software/team118/maf_stream/target/release/maf_stream coverage $REF coverage/$REF/$REF.random.intervals.$num.maf coverage/$REF/$REF.random.intervals.$num.maf.cov;done
-
-# Plot coverage (boxplot)
-# Ideally the species reported in the species_group.tsv file should follow the order of the phylogenetic tree
-python3 plot_coverage.py --cov coverage/$REF --refGenome $REF --species species_group.tsv
+# Plot coverage
+python3 plot_coverage.py --species_list ../lepidoptera/89way/version_202201/species/species_list_012022_v2.tsv --cov coverage/$REF
+--t ../lepidoptera/89way/guide_tree/supermatrix_datafreeze_080621.treefile.pruned --o coverage/$REF --refGenome $REF
 
