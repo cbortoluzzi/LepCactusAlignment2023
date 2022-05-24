477b89ff996b000f67d53183c398ef2763a27e6a
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-05-24 14:49:09 +0100

    Update quality_check_busco.sh

--- qc_cactus_alignment/quality_check_busco.sh
+++ qc_cactus_alignment/quality_check_busco.sh
@@ -16,8 +16,10 @@ REFGENOME=$1
 HAL=$2
 OUTPUT=$3
 
+
 # Obtain pairwise comparisons between query and target species
 python3 consistency.py --refGenome $REFGENOME --list_genes final_busco_ids.txt --species_list species_list.tsv --tree supermatrix_datafreeze_080621.treefile.pruned --hal $HAL --o $OUTPUT
+
 
 # Plot consistent single-copy BUSCO genes per species, as well as inconsistent genes
 python3 plot_consistency.py --d busco_quality_check/ --tree supermatrix_datafreeze_080621.treefile.pruned --refGenome $REFGENOME
