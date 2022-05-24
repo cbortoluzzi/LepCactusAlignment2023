26e780ef0cc026d6e64d33a6dcb0b25ecdddd8c6
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-05-24 15:56:35 +0100

    Update plot_PHRED_RD_GQ_distribution.py

--- PopGen/plot_PHRED_RD_GQ_distribution.py
+++ PopGen/plot_PHRED_RD_GQ_distribution.py
@@ -16,13 +16,23 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description = '')
 parser.add_argument('--vcf', help = 'VCF file')
-parser.add_argument('--species' , help = 'Species name')
-parser.add_argument('--group' , help = 'Group the species belongs to')
+parser.add_argument('--species_list', help = 'Tab delimited species file')
 parser.add_argument('--o', help = 'Output directory')
 
 
 
-def plot_vcf_statistics(vcf_f, sample, output_directory, species, color):
+def get_species_group(species_list):
+	mygenome = {}
+	with open(species_list) as f:
+		for line in f:
+			assembly, tol_id, p_class, species_name, group = line.strip().split()
+			tol = tol_id.split('.')[0]
+			mygenome[tol] = [group, species_name]
+	return mygenome
+
+
+
+def plot_vcf_statistics(vcf_f, tol_vcf, mygenome, output_directory):
 	vcf_reader = vcf.Reader(filename=vcf_f)
 	phred_scaled_quality_score, read_depth, genotype_quality_score = [], [], []
 	for record in vcf_reader:
@@ -32,17 +42,20 @@ def plot_vcf_statistics(vcf_f, sample, output_directory, species, color):
 				genotype = call['GT']
 				genotype_quality = call['GQ']
 				genotype_depth = call['DP']
-				if genotype == '0/1' or genotype == '1/1':
+				if genotype == '0/1':
 					phred_scaled_quality_score.append(record.QUAL)
 					read_depth.append(genotype_depth)
 					genotype_quality_score.append(genotype_quality)
-
-	fig = Path(output_directory, sample + '.qual.pdf')
-	plot_phred_score = plot_histogram_distribution(phred_scaled_quality_score, species, 'PHRED-quality score', fig, color)
-	fig = Path(output_directory, sample + '.dp.pdf')
-	plot_read_depth = plot_histogram_distribution(read_depth, species, 'Read depth', fig, color)
-	fig = Path(output_directory, sample + '.gq.pdf')
-	plot_genotype_quality = plot_histogram_distribution(genotype_quality_score, species, 'Genotype quality', fig, color)
+	if mygenome[tol_vcf][0] == 'Moth':
+		color = '#2f6694'
+	else:
+		color = '#ff8000'
+	fig = Path(output_directory, mygenome[tol_vcf][1] + '.PHRED.pdf')
+	plot_phred_score = plot_histogram_distribution(phred_scaled_quality_score, mygenome[tol_vcf][1], 'PHRED-quality score', fig, color)
+	fig = Path(output_directory, mygenome[tol_vcf][1] + '.DP.pdf')
+	plot_read_depth = plot_histogram_distribution(read_depth, mygenome[tol_vcf][1], 'Read depth', fig, color)
+	fig = Path(output_directory, mygenome[tol_vcf][1] + '.GQ.pdf')
+	plot_genotype_quality = plot_histogram_distribution(genotype_quality_score, mygenome[tol_vcf][1], 'Genotype quality', fig, color)
 
 
 
@@ -63,8 +76,9 @@ if __name__ == "__main__":
 	sample = Path(args.vcf).name.split('.')[0]
 	path = Path(args.o)
 	path.mkdir(parents=True, exist_ok=True)
-	if args.group == 'Moth' or args.group == 'moth':
-		color = '#2f6694'
-	else:
-		color = '#ff8000'
-	statistics = plot_vcf_statistics(args.vcf, sample, path, args.species, color)
+	species_group = get_species_group(args.species_list)
+	tol_vcf = Path(args.vcf).stem.split('.')[0]
+	statistics = plot_vcf_statistics(args.vcf, tol_vcf, species_group, path)
+
+	
+	
