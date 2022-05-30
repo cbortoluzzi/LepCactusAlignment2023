9767bca585b8ada28633bca0111316083174e42e
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-05-30 12:11:48 +0100

    Update quality_check_busco.sh

--- qc_cactus_alignment/quality_check_busco.sh
+++ qc_cactus_alignment/quality_check_busco.sh
@@ -7,21 +7,22 @@
 
 
 if [ -z $1 ]; then
-        echo "Usage: ./quality_check_busco.sh <name of reference genome> <input hal file> <output directory>"
+        echo "Usage: ./quality_check_busco.sh <name of query species> <input hal file> <name of output directory>"
         exit -1
 fi
 
 
-REFGENOME=$1
+QUERY=$1
 HAL=$2
 OUTPUT=$3
 
 
 # Obtain pairwise comparisons between query and target species
-python3 consistency.py --refGenome $REFGENOME --list_genes final_busco_ids.txt --species_list species_list.tsv --tree supermatrix_datafreeze_080621.treefile.pruned --hal $HAL --o $OUTPUT
+# In our study, the query species was Tinea_trinotella, the most basal of the lepidoptera species
+python3 consistency.py --refGenome $QUERY --list_genes final_busco_ids.txt --species_list species_list.tsv --tree supermatrix_datafreeze_080621.treefile.pruned --hal $HAL --o $OUTPUT
 
 
 # Plot consistent single-copy BUSCO genes per species, as well as inconsistent genes
-python3 plot_consistency.py --d busco_quality_check/ --tree supermatrix_datafreeze_080621.treefile.pruned --refGenome $REFGENOME
+python3 plot_consistency.py --d busco_quality_check/ --tree supermatrix_datafreeze_080621.treefile.pruned --refGenome $QUERY
 
 
