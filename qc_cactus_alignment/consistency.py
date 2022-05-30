5cd8b5a1f0adaca625f479373c38021d59271ce7
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-05-30 12:15:11 +0100

    Update consistency.py

--- qc_cactus_alignment/consistency.py
+++ qc_cactus_alignment/consistency.py
@@ -16,7 +16,7 @@ from collections import defaultdict
 
 
 parser = argparse.ArgumentParser(description = 'Check consistency of BUSCO genomic coordinates with cactus alignment')
-parser.add_argument('--refGenome', help = 'Name of reference species. We recommend to use the outgroup or the most basal species')
+parser.add_argument('--refGenome', help = 'Name of query species. We recommend to use the outgroup or the most basal species')
 parser.add_argument('--list_genes', help = 'List of single-copy BUSCO genes, one per line')
 parser.add_argument('--species_list', help = 'List of species, one per line')
 parser.add_argument('--tree', help = 'Phylogenetic tree used as guide tree in cactus')
@@ -34,7 +34,6 @@ def pairwise_comparisons(refGenome, tree, path):
 	for node in phylo.traverse('postorder'):
 		if node.is_leaf():
 			if node.name != refGenome and node.name != "Hydropsyche_tenuis":
-				# Phylogenetic distance
 				output_directory = Path(path, refGenome + '_vs_' + node.name)
 				output_directory.mkdir(parents=True, exist_ok=True)
 				list_nodes.append(node.name)
@@ -47,7 +46,7 @@ def get_species_name_tol_id(species_list):
 	species_d = {}
 	with open(species_list) as f:
 		for line in f:
-			assembly, tol_id, p_class, species_name, group = line.strip().split()
+			assembly, tol_id, p_class, species_name, superfamily = line.strip().split()
 			species_d[species_name] = [tol_id, assembly]
 	return species_d
 
