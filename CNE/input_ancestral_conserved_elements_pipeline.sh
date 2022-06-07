51697330491757acad4c1d6e24285806840e2395
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-06-07 13:34:22 +0100

    Update and rename get_sequences_genomes.sh to input_ancestral_conserved_elements_pipeline.sh

--- CNE/input_ancestral_conserved_elements_pipeline.sh
+++ CNE/input_ancestral_conserved_elements_pipeline.sh
@@ -7,7 +7,7 @@
 
 
 if [ -z $1 ]; then
-        echo "Usage: ./get_sequences_genomes.sh <reference.genome> <cactus.alignment>"
+        echo "Usage: ./input_ancestral_conserved_elements_pipeline.sh <name of reference genome> <input hal file>"
         exit -1
 fi
 
@@ -16,14 +16,13 @@ REF=$1
 HAL=$2
 
 
-# Print sequences of a given genome in bed format (in our case the genome is the MRCA). We will retain only sequences with a minimum length of 50 bp 
-echo "halStats --bedSequences $REF $HAL"
+
+# Print sequences of a given genome in bed format (in our case the genome is the MRCA). We will retain only sequences with a minimum length of 50 bp
 halStats --bedSequences $REF $HAL | awk '{if($3 >= 50)print}' | sort -k3,3 -nr > $REF.sequences.50bp.bed
-split -l 300 $REF.sequences.50bp.bed output_file
+split -l 300 $REF.sequences.50bp.bed sequences
 
 
 # Print the list of genomes in the alignment
-echo "halStats --genome $HAL"
 halStats --genomes $HAL > genomes.txt
 for species in `cat genomes.txt`; do echo $species | grep -v '^Anc';done > genomes_noAncestors.txt
-
+rm genomes.txt
