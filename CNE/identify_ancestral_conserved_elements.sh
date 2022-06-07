64c8b947c7b646caa3595b5e2bb4cf1860361a11
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-06-07 13:34:53 +0100

    Update identify_ancestral_conserved_elements.sh

--- CNE/identify_ancestral_conserved_elements.sh
+++ CNE/identify_ancestral_conserved_elements.sh
@@ -7,7 +7,7 @@
 
 
 if [ -z $1 ]; then
-        echo "Usage: ./identify_ancestral_conserved_elements.sh <reference.genome> <cactus.alignment> <reference.sequence>"
+        echo "Usage: ./identify_ancestral_conserved_elements.sh <name of reference genome> <input hal file> <name of reference sequence within reference genome>"
         exit -1
 fi
 
@@ -18,13 +18,10 @@ CONTIG=$3
 
 
 mkdir -p $CONTIG
-cat genomes_noAncestors.txt | while read targetGenome
-do
-        echo "Analysing species ..." $targetGenome
+targetGenomes=`awk '{print $1}' genomes_noAncestors.txt | paste -s -d, -`
 
-        echo "hal2maf --refGenome $REF --refSequence $CONTIG --onlyOrthologs --targetGenomes $targetGenome $HAL $CONTIG/$targetGenome.maf"
-        hal2maf --refGenome $REF --refSequence $CONTIG --onlyOrthologs --targetGenomes $targetGenome $HAL $CONTIG/$targetGenome.maf
+# Get alignment
+hal2maf --refGenome $REF --refSequence $CONTIG --onlyOrthologs --targetGenomes $targetGenomes $HAL $CONTIG/$CONTIG.maf
+# Identify ancestral conserved elements
+python3 ancestral_conserved_elements_v2.py --maf $CONTIG/$CONTIG.maf --refGenome $REF --refSequence $CONTIG --score 70 --length 50
 
-        echo "python3 ancestral_conserved_elements.py -maf $CONTIG/$targetGenome.maf -refGenome $REF -refSequence $CONTIG"
-        python3 ancestral_conserved_elements.py --maf $CONTIG/$targetGenome.maf --refGenome $REF --refSequence $CONTIG
-done
