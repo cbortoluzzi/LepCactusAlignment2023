fe8c9ae47731ff9ba3c4d34dadbca6992f90b76f
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-06-09 17:42:04 +0100

    Update run_orthofinder.sh

--- qc_cactus_alignment/run_orthofinder.sh
+++ qc_cactus_alignment/run_orthofinder.sh
@@ -9,21 +9,27 @@ export PATH=/lustre/scratch123/tol/teams/durbin/users/cb46/softwares/OrthoFinder
 
 
 if [ -z $1 ]; then
-        echo "Usage: ./run_orthofinder.sh"
+        echo "Usage: ./run_orthofinder.sh <a tab delimited species list file>"
         exit -1
 fi
 
 
+FILE=$1
+
+
 mkdir -p orthofinder/proteomes
-cat species.tsv | while read assembly tol_id class species_name superfamily
+
+
+cat $FILE | while read assembly tol_id class species_name superfamily
 do
   # Copy proteome file 
   cp -r /lustre/scratch123/tol/projects/lepidoptera/data/insects/$species_name/analysis/$tol_id/gene/ensembl/latest/assembly.ensembl.pep.fa.gz orthofinder/proteomes/$species_name.pep.fa.gz
   # Unzip proteome file
   gunzip orthofinder/proteomes/$species_name.pep.fa.gz
   # Obtain primary transcript
-  python /lustre/scratch123/tol/teams/durbin/users/cb46/softwares/OrthoFinder/tools/primary_transcript.py orthofinder/proteomes/$species_name.pep.fa
+  python3 primary_transcript.py --pep orthofinder/proteomes/$species_name.pep.fa
 done
 
 # Run OrthoFinder on all primary transcripts of all 49 lepidoptera species
 orthofinder -f orthofinder/proteomes/primary_transcripts/
+
