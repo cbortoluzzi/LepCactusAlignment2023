e5e0881647928e9708a194543ce3f79b9be7e7dd
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-06-09 17:39:33 +0100

    Update primary_transcript.py

--- qc_cactus_alignment/primary_transcript.py
+++ qc_cactus_alignment/primary_transcript.py
@@ -11,21 +11,11 @@ from collections import defaultdict
 
 
 
-parser = argparse.ArgumentParser(description = 'Select longest transcript from a multi fasta file')
+parser = argparse.ArgumentParser(description = 'Select longest transcript of a gene as primary transcript')
 parser.add_argument('--pep', help = 'Proteome in FASTA format')
 
 
-def select_primary_transcript(pep_f):
-	# Set output directory and output file
-	path = Path(pep_f).parents[0]
-	file_name = Path(pep_f).name
-	# Create folders in case they don't exist
-	# One folder for the longest transcripts
-	longest_transcript_output_directory = Path(path, 'primary_transcripts')
-	longest_transcript_output_directory.mkdir(parents=True, exist_ok=True)
-	# One folder for the genomic coordinates of the longest transcript (these files will be used later on in the quality check step)
-	genomic_coordinates_longest_transcript = Path(path, 'primary_transcripts_coordinates')
-	genomic_coordinates_longest_transcript.mkdir(parents=True, exist_ok=True)
+def select_primary_transcript(pep_f, file_name, path_1, path_2):
 	longest_transcript = defaultdict(list)
 	# Parse proteome in FASTA format
 	for record in SeqIO.parse(pep_f, "fasta"):
@@ -35,7 +25,9 @@ def select_primary_transcript(pep_f):
 		len = int(end) - int(start)
 		seq = str(record.seq)
 		longest_transcript[gene].append([chromosome, start, end, len, seq])
-	with open(Path(longest_transcript_output_directory, file_name), 'w') as fasta, open(Path(genomic_coordinates_longest_transcript, file_name.replace('.pep.fa', '.bed')), 'w') as bed:
+
+	genomic_coordinates_f = file_name.replace('.pep.fa', '.bed')
+	with open(Path(path_1, file_name), 'w') as fasta, open(Path(path_2, genomic_coordinates_f), 'w') as bed:
 		for gene_name in longest_transcript:
 			# Select longest transcript
 			longest = max(longest_transcript[gene_name], key=lambda a: a[3])
@@ -46,7 +38,15 @@ def select_primary_transcript(pep_f):
 
 if __name__ == "__main__":
 	args = parser.parse_args()
-	primary_transcript = select_primary_transcript(args.pep)
+	path = Path(args.pep).parents[0]
+	file_name = Path(args.pep).name
+	# Create folder where to print primary transcripts
+	longest_transcript_output_directory = Path(path, 'primary_transcripts')
+	longest_transcript_output_directory.mkdir(parents=True, exist_ok=True)
+	# Create folder where to print the genomic coordinates of the longest transcript
+	genomic_coordinates_longest_transcript = Path(path, 'primary_transcripts_coordinates')
+	genomic_coordinates_longest_transcript.mkdir(parents=True, exist_ok=True)
+	# Identify longest transcript
+	primary_transcript = select_primary_transcript(args.pep, file_name, longest_transcript_output_directory, genomic_coordinates_longest_transcript)
 
-  
   
