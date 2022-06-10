1f28a07601bbc2cf1d4dc7682445db56e3f99151
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-06-10 09:33:45 +0100

    Update quality_check_busco.sh

--- qc_cactus_alignment/quality_check_busco.sh
+++ qc_cactus_alignment/quality_check_busco.sh
@@ -7,7 +7,7 @@
 
 
 if [ -z $1 ]; then
-        echo "Usage: ./quality_check_busco.sh <name of query species> <input hal file> <name of output directory>"
+        echo "Usage: ./quality_check_busco.sh <name of species to use as query> <input hal file> <name of output directory>"
         exit -1
 fi
 
@@ -17,12 +17,11 @@ HAL=$2
 OUTPUT=$3
 
 
-# Obtain pairwise comparisons between query and target species
-# In our study, the query species was Tinea_trinotella, the most basal of the lepidoptera species
+# Obtain pairwise comparisons between query (in our case Tinea trinotella) and target species
 python3 consistency.py --refGenome $QUERY --list_genes final_busco_ids.txt --species_list species_list.tsv --tree supermatrix_datafreeze_080621.treefile.pruned --hal $HAL --o $OUTPUT
 
 
-# Plot consistent single-copy BUSCO genes per species, as well as inconsistent genes
-python3 plot_consistency.py --d busco_quality_check/ --tree supermatrix_datafreeze_080621.treefile.pruned --refGenome $QUERY
+# Plot consistent of single-copy BUSCO genes per species, as well as inconsistent genes
+python3 plot_consistency.py --d busco_quality_check/ --tree supermatrix_datafreeze_080621.treefile.pruned --refGenome $QUERY --species_list species_list.tsv --o busco_quality_check
 
 
