7784353bb5f85cf92845c1f1b90e6e4d1cc05e2e
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-07-20 12:22:46 +0200

    Create genomic_evolutionary_rate_profiling.sh

---
+++ GERP/genomic_evolutionary_rate_profiling.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+
+# Author: @cb46
+
+
+
+export PATH=/software/team118/maf_stream/target/release:$PATH
+
+
+
+if [ -z $1 ]; then
+        echo "Usage: ./genomic_evolutionary_rate_profiling.sh <input maf file>"
+        exit -1
+fi
+
+
+maf=$1
+
+
+
+## Select sites with at least 3 ungapped sequences
+genome=$(basename $maf .maf | cut -f1 -d'.')
+#mkdir -p bed/$genome
+#python3 select_sites_with_ungapped_species.py --maf $maf --o bed/$genome
+#echo "Done with selecting sites"
+
+
+## Filter input maf file
+bed=$(basename $maf .maf)
+mkdir -p GERP++/$genome
+maf_stream filter --bed bed/$genome/$bed.bed $maf GERP++/$genome/$bed.maf
+echo "Done with maf_stream"
+
+
+## Run GERP++
+chromosome=$(basename $maf .maf | cut -f2 -d'.')
+re='^[0-9]+$'
+if [[ $chromosome =~ $re ]]
+then
+        cat neutral_mod/$genome/$genome.ave.nonconserved-4d.mod | grep 'TREE:' | sed 's/TREE: //g' > tree.$genome.ave.nh
+        gerpcol -t tree.$genome.ave.nh -f GERP++/$genome/$bed.maf -e $genome -j
+elif [[ $chromosome =~ "Z" ]]
+then
+        cat neutral_mod/$genome/$genome.Z.nonconserved-4d.mod | grep 'TREE:' | sed 's/TREE: //g' > tree.$genome.Z.nh
+        gerpcol -t tree.$genome.Z.nh -f GERP++/$genome/$bed.maf -e $genome -j
+elif [[ $chromosome =~ "W" ]]
+then
+        cat neutral_mod/$genome/$genome.W.nonconserved-4d.mod | grep 'TREE:' | sed 's/TREE: //g' > tree.$genome.W.nh
+        gerpcol -t tree.$genome.W.nh -f GERP++/$genome/$bed.maf -e $genome -j
+fi
+echo "Done with GERP++"
+
