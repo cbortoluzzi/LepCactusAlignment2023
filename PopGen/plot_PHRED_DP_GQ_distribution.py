5146cb31a2fea2af7a5ad806ff7175a20820daee
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-08-16 10:41:20 +0200

    Update plot_PHRED_DP_GQ_distribution.py

--- PopGen/plot_PHRED_DP_GQ_distribution.py
+++ PopGen/plot_PHRED_DP_GQ_distribution.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 
 
-parser = argparse.ArgumentParser(description = '')
+parser = argparse.ArgumentParser(description = 'Plot distribution of phred-quality score, read depth, and genotype quality')
 parser.add_argument('--vcf', help = 'VCF file')
 parser.add_argument('--species_name', help = 'Species name')
 parser.add_argument('--o', help = 'Output directory')
@@ -25,22 +25,25 @@ def plot_vcf_statistics(vcf_f, species_name, output_file, output_directory):
 	vcf_reader = vcf.Reader(filename=vcf_f)
 	phred_scaled_quality_score, read_depth, genotype_quality_score = [], [], []
 	for record in vcf_reader:
-		# let's consider only bi-allelic SNPs that pass all filtering criteria
+		# Let's consider only bi-allelic SNPs
 		if not record.FILTER and record.is_snp:
 			for call in record.samples:
 				genotype = call['GT']
 				genotype_quality = call['GQ']
 				genotype_depth = call['DP']
+				# Let's consider only heterozygous sites
 				if genotype == '0/1':
 					phred_scaled_quality_score.append(record.QUAL)
 					read_depth.append(genotype_depth)
 					genotype_quality_score.append(genotype_quality)
 
-
+	# Distribution of phred-quality score
 	fig = Path(output_directory, output_file + '.QUAL.pdf')
 	plot_phred_score = plot_histogram_distribution(phred_scaled_quality_score, species_name, 'PHRED-quality score', fig)
+	# Distribution of read depth
 	fig = Path(output_directory, output_file + '.DP.pdf')
 	plot_read_depth = plot_histogram_distribution(read_depth, species_name, 'Read depth', fig)
+	# Distribution of genotype quality
 	fig = Path(output_directory, output_file + '.GQ.pdf')
 	plot_genotype_quality = plot_histogram_distribution(genotype_quality_score, species_name, 'Genotype quality', fig)
 
@@ -64,4 +67,5 @@ if __name__ == "__main__":
 	path.mkdir(parents=True, exist_ok=True)
 	output_file = Path(args.vcf).stem
 	statistics = plot_vcf_statistics(args.vcf, args.species_name, output_file, path)
-
+	
+	
