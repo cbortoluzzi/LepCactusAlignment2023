42737486f0702eabaf6708136ca234b483bb48ab
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-08-30 15:19:26 +0200

    Update genomes.sh

--- qc_cactus_alignment/genomes.sh
+++ qc_cactus_alignment/genomes.sh
@@ -12,9 +12,9 @@ if [ -z $1 ]; then
 fi
 
 
-HAL=$1
+hal=$1
 
 
 # Print list of species in the alignment
-halStats --genomes $HAL | tr -s ' '  '\n' | grep -v '^Anc' | sort > genomes_noAncestors.txt
+halStats --genomes $hal | tr -s ' '  '\n' | grep -v '^Anc' | sort > genomes_noAncestors.txt
 
