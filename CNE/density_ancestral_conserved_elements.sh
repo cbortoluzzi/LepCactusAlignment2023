169b7f5b6f0c01cceda7973236722f6a768a6672
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-09-05 15:21:00 +0200

    Update density_ancestral_conserved_elements.sh

--- CNE/density_ancestral_conserved_elements.sh
+++ CNE/density_ancestral_conserved_elements.sh
@@ -9,24 +9,24 @@ export PATH=/lustre/scratch123/tol/teams/durbin/users/cb46/softwares:$PATH
 
 
 if [ -z $1 ]; then
-        echo "Usage: ./density.sh <ancestral conserved elements> <genome file size>"
-        exit -1
+	echo "Usage: ./density.sh <ancestral conserved elements> <genome file size>"
+	exit -1
 fi
 
 
-ANCESTRAL_F=$1
-GENOME=$2
+ancestral_f=$1
+genome=$2
 
 
 mkdir -p ancestral_conserved_elements_density
-species_name=$(basename $ANCESTRAL_F | sed 's/ancestral_conserved_elements_//g' | sed 's/.txt//g')
 
-# Calculate density in N-bp window
-bedtools makewindows -g $GENOME -w 100000 > ancestral_conserved_elements_density/$species_name.window.bed
+species_name=$(basename $ancestral_f .bed)
+
+# Calculate density in N bp window: we are using a window size of 100 kb, but this number can be changed
+bedtools makewindows -g $genome -w 100000 > ancestral_conserved_elements_density/$species_name.window.bed
 # We will consider only ancestral conserved elements that do not contain repeats
-cat $ANCESTRAL_F | awk '{if($5 == 0.0)print}' | bedtools coverage -a ancestral_conserved_elements_density/$species_name.window.bed -b stdin > ancestral_conserved_elements_density/$species_name.100kb.bed
+cat $ancestral_f | awk 'OFS="\t"{if($7 == 0.0)print $1,$2,$2+$3}' | bedtools coverage -a ancestral_conserved_elements_density/$species_name.window.bed -b stdin > ancestral_conserved_elements_density/$species_name.100kb.bed
 rm ancestral_conserved_elements_density/$species_name.window.bed
 
-# Plot density distribution of ancestral conserved elements
-species=`echo $species_name | cut -f1,2 -d'_' | sed 's/.*/\u&/'`
-/software/R-4.0.3/lib/R/bin/Rscript plot_density_ancestral_conserved_elements.r ancestral_conserved_elements_density/$species_name.100kb.bed $species
+# Average density in N bp window
+cat ancestral_conserved_elements_density/$species_name.100kb.bed | awk '{sum+=$4}END{print sum/NR}'
