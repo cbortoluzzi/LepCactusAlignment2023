bc18a6e43d0daf560fb5a400bbf9d3a384d79b4b
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-09-07 11:26:45 +0200

    Update input_ancestral_conserved_elements_pipeline.sh

--- CNE/input_ancestral_conserved_elements_pipeline.sh
+++ CNE/input_ancestral_conserved_elements_pipeline.sh
@@ -19,10 +19,11 @@ HAL=$2
 
 # Print sequences of a given genome in bed format (in our case the genome is the MRCA
 halStats --bedSequences $REF $HAL | sort -k3,3 -nr > $REF.sequences.bed
-split -l 300 $REF.sequences.bed sequences
+split -l 300 $REF.sequences.bed sequences_
 
 
 # Print the list of genomes in the alignment
 halStats --genomes $HAL > genomes.txt
 for species in `cat genomes.txt`; do echo $species | grep -v '^Anc';done > genomes_noAncestors.txt
 rm genomes.txt
+
