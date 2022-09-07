7a7554637a442a6602e54d19ba52eb76cccef36b
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-09-07 11:26:12 +0200

    Update density_ancestral_conserved_elements.sh

--- CNE/density_ancestral_conserved_elements.sh
+++ CNE/density_ancestral_conserved_elements.sh
@@ -9,7 +9,7 @@ export PATH=/lustre/scratch123/tol/teams/durbin/users/cb46/softwares:$PATH
 
 
 if [ -z $1 ]; then
-	echo "Usage: ./density.sh <ancestral conserved elements> <genome file size>"
+	echo "Usage: ./density_ancestral_conserved_elements.sh <ancestral conserved elements> <genome file size>"
 	exit -1
 fi
 
@@ -18,15 +18,15 @@ ancestral_f=$1
 genome=$2
 
 
-mkdir -p ancestral_conserved_elements_density
+mkdir -p LSHCE_density
 
-species_name=$(basename $ancestral_f .bed)
+species_name=$(basename $ancestral_f .bed | cut -f1 -d'.')
 
 # Calculate density in N bp window: we are using a window size of 100 kb, but this number can be changed
-bedtools makewindows -g $genome -w 100000 > ancestral_conserved_elements_density/$species_name.window.bed
+bedtools makewindows -g $genome -w 100000 > LSHCE_density/$species_name.window.bed
 # We will consider only ancestral conserved elements that do not contain repeats
-cat $ancestral_f | awk 'OFS="\t"{if($7 == 0.0)print $1,$2,$2+$3}' | bedtools coverage -a ancestral_conserved_elements_density/$species_name.window.bed -b stdin > ancestral_conserved_elements_density/$species_name.100kb.bed
-rm ancestral_conserved_elements_density/$species_name.window.bed
+cat $ancestral_f | awk 'OFS="\t"{print $1,$2,$2+$3}' | bedtools coverage -a LSHCE_density/$species_name.window.bed -b stdin > LSHCE_density/$species_name.100kb.bed
+rm LSHCE_density/$species_name.window.bed
 
 # Average density in N bp window
-cat ancestral_conserved_elements_density/$species_name.100kb.bed | awk '{sum+=$4}END{print sum/NR}'
+cat LSHCE_density/$species_name.100kb.bed | awk '{sum+=$4}END{print sum/NR}'
