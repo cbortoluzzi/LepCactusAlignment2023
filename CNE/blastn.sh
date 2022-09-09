639758c33773a28b175ec2d057d131e0ff204776
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-09-09 10:44:04 +0200

    Update blastn.sh

--- CNE/blastn.sh
+++ CNE/blastn.sh
@@ -11,7 +11,7 @@ mkdir -p blastn
 for f in ancestral_conserved_elements/*.bed
 do
 	genome=$(basename $f .bed)
-	#cat $f | awk '{print ">"$1":"$2"-"$2+$3"\n"$8}' > blastn/$genome.fa
+	cat $f | awk '{print ">"$1":"$2"-"$2+$3"\n"$8}' > blastn/$genome.fa
 	# Run Blastn on query
 	echo "Running blastn on" $genome
 	blastn -db db/outgroups -query blastn/$genome.fa -outfmt 6 -out blastn/$genome.blastn.out
