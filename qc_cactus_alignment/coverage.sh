a1f75eca17887707af224b6789149017fe02a20a
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-10-27 15:05:18 +0100

    Update coverage.sh

--- qc_cactus_alignment/coverage.sh
+++ qc_cactus_alignment/coverage.sh
@@ -5,7 +5,9 @@
 # Author: @cb46
 
 
+
 export PATH=/software/team118/maf_stream/target/release:$PATH
+
 
 
 if [ -z $1 ]; then
@@ -14,30 +16,38 @@ if [ -z $1 ]; then
 fi
 
 
-HAL=$1
-REF=$2
+
+hal=$1
+genome=$2
 
 
-mkdir -p coverage/$REF
+mkdir -p coverage/$genome
 
 
-# Print sequences of given genome in BED format 
-halStats --bedSequences $REF $HAL | cut -f1,3 > coverage/$REF/$REF.genome
+re='^[0-9]+$'
+# Print sequences of given genome in BED format
+halStats --bedSequences $genome $hal | cut -f1,3 | sort -k1,1 -k2,2n | while read chromosome length
+do
+	if [[ $chromosome =~ $re ]] || [[ $chromosome =~ "W" ]] || [[ $chromosome =~ "Z" ]]
+	then
+		printf "%s\t%s\n" $chromosome $length >> coverage/$genome/$genome.txt
+	fi
+done
 
 
 # Generate 100 random intervals, each 1,000,000 bp long
-bedtools random -g coverage/$REF/$REF.genome -l 1000000 -seed 12345 -n 100 > coverage/$REF/$REF.random.intervals
+bedtools random -g coverage/$genome/$genome.txt -l 1000000 -seed 12345 -n 100 > coverage/$genome/$genome.random.intervals
 
 
 # Convert HAL database to an alignment in multiple alignment format (MAF)
-cat coverage/$REF/$REF.random.intervals | while read contig start end num length strand
+cat coverage/$genome/$genome.random.intervals | while read chromosome start end num length strand
 do
-        hal2maf --refSequence $contig --start $start --length 1000000 --refGenome $REF --onlyOrthologs --noAncestors $HAL coverage/$REF/$REF.random.intervals.$num.maf
-        # Calculate coverage
-        maf_stream coverage $REF coverage/$REF/$REF.random.intervals.$num.maf coverage/$REF/$REF.random.intervals.$num.maf.cov
+	hal2maf --refSequence $chromosome --start $start --length $length --refGenome $genome --onlyOrthologs --noAncestors $hal coverage/$genome/$genome.random.intervals.$num.maf
+	# Calculate coverage
+	maf_stream coverage $genome coverage/$genome/$genome.random.intervals.$num.maf coverage/$genome/$genome.random.intervals.$num.maf.cov
 done
 
-# Plot coverage
-python3 plot_coverage.py --species_list species_list.tsv --cov coverage/$REF --t supermatrix_datafreeze_080621.treefile.pruned --o coverage/$REF --refGenome $REF
 
+# Plot coverage
+python3 plot_coverage.py --t ../phylogenetic_tree.nh --l 1000000 --c coverage/$genome --f ../species_list.tsv --refGenome $genome --o coverage/$genome
 
