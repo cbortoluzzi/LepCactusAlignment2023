63356867eef83ac237b5ea9ff974827ce361d11a
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2022-10-31 20:39:01 +0000

    Update genomic_evolutionary_rate_profiling.sh

--- GERP/genomic_evolutionary_rate_profiling.sh
+++ GERP/genomic_evolutionary_rate_profiling.sh
@@ -1,63 +1,48 @@
 #!/bin/bash
 
 
-
 # Author: @cb46
 
 
-
-export PATH=/software/team118/maf_stream/target/release:$PATH
-
-
-
 if [ -z $1 ]; then
-        echo "Usage: ./genomic_evolutionary_rate_profiling.sh <input maf file>"
+        echo "Usage: ./genomic_evolutionary_rate_profiling.sh <name of reference genome>"
         exit -1
 fi
 
 
-maf=$1
+genome=$1
 
 
-
-## Select sites with at least 3 ungapped sequences
-genome=$(basename $maf .maf | cut -f1 -d'.')
-mkdir -p bed/$genome
-python3 select_sites_with_ungapped_species.py --maf $maf --o bed/$genome
-echo "Done with selecting sites"
+mkdir -p bed/$genome && mkdir -p GERP++/$genome
 
 
-## Filter input maf file
-bed=$(basename $maf .maf)
-mkdir -p GERP++/$genome
-maf_stream filter --bed bed/$genome/$bed.bed $maf GERP++/$genome/$bed.maf
-echo "Done with maf_stream"
-
-
-## Run GERP++
-chromosome=$(basename $maf .maf | cut -f2 -d'.')
 re='^[0-9]+$'
-if [[ $chromosome =~ $re ]]
-then
-        cat neutral_mod/$genome/$genome.ave.nonconserved-4d.mod | grep 'TREE:' | sed 's/TREE: //g' > tree.$genome.ave.nh
-        gerpcol -t tree.$genome.ave.nh -f GERP++/$genome/$bed.maf -e $genome -j
-elif [[ $chromosome =~ "Z" ]]
-then
-        cat neutral_mod/$genome/$genome.Z.nonconserved-4d.mod | grep 'TREE:' | sed 's/TREE: //g' > tree.$genome.Z.nh
-        gerpcol -t tree.$genome.Z.nh -f GERP++/$genome/$bed.maf -e $genome -j
-elif [[ $chromosome =~ "W" ]]
-then
-        cat neutral_mod/$genome/$genome.W.nonconserved-4d.mod | grep 'TREE:' | sed 's/TREE: //g' > tree.$genome.W.nh
-        gerpcol -t tree.$genome.W.nh -f GERP++/$genome/$bed.maf -e $genome -j
-fi
-echo "Done with GERP++"
+for maf in maf/$genome/*.maf
+do
+        echo "Select sites with at least N species without gaps"
+        python3 select_sites_with_ungapped_species.py --maf $maf --n 3 --o bed/$genome
 
+        echo "Filter multiple sequence alignment"
+        maf_stream filter --bed bed/$genome/$(basename $maf .maf).bed $maf GERP++/$genome/$(basename $maf)
 
-## Obtain the position for each rejected substitution score in the multiple sequence alignment
-python3 gerp_score_per_nucleotide_in_alignment.py --maf GERP++/$genome/$bed.maf
+        echo "Run GERP++"
+        chromosome=$(basename $maf .maf | cut -f2 -d'.')
+        if [[ $chromosome =~ $re ]]
+        then
+                cat nmodel/$genome/$genome.ave.nonconserved-4d.mod | grep 'TREE:' | sed 's/TREE: //g' > nmodel/$genome/$genome.ave.nh
+                gerpcol -t nmodel/$genome/$genome.ave.nh -f GERP++/$genome/$(basename $maf) -e $1 -j
+                python3 gerp_score_per_nucleotide_in_alignment.py --maf GERP++/$genome/$(basename $maf)
+        elif [[ $chromosome =~ "Z" ]]
+        then
+                cat nmodel/$genome/$genome.Z.nonconserved-4d.mod | grep 'TREE:' | sed 's/TREE: //g' > nmodel/$genome/$genome.Z.nh
+                gerpcol -t nmodel/$genome/$genome.Z.nh -f GERP++/$genome/$(basename $maf) -e $1 -j
+                python3 gerp_score_per_nucleotide_in_alignment.py --maf GERP++/$genome/$(basename $maf)
+        else
+                cat nmodel/$genome/$genome.W.nonconserved-4d.mod | grep 'TREE:' | sed 's/TREE: //g' > nmodel/$genome/$genome.W.nh
+                gerpcol -t nmodel/$genome/$genome.W.nh -f GERP++/$genome/$(basename $maf) -e $1 -j
+                python3 gerp_score_per_nucleotide_in_alignment.py --maf GERP++/$genome/$(basename $maf)
+        fi
+done
 
-
-## Filter out sites that do evolve neutrally (GERP score < 0)
-cat GERP++/$genome/$bed.maf.rates.bed | awk '{if($6 >= 0)print}' > GERP++/$genome/$bed.maf.rates.conserved.bed
-
+rm nmodel/$genome/$genome.*.nh
 
