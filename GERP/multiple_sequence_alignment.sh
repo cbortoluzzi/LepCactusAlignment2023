29d1c193d6e62b548610c21d2a3fb8fad8846792
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-03-07 14:14:10 +0000

    Update multiple_sequence_alignment.sh

--- GERP/multiple_sequence_alignment.sh
+++ GERP/multiple_sequence_alignment.sh
@@ -14,32 +14,18 @@ genome=$1
 hal=$2
 
 
-echo "./multiple_sequence_alignment.sh $genome $hal"
-
 
 mkdir -p maf/$genome
 
 
-re='^[0-9]+$'
-
 echo "Print sequences of given genome in bed format"
-halStats --bedSequences $genome $hal | cut -f1,3 | sort -k1,1 -k2,2n | while read chromosome length
-do
-        if [[ $chromosome =~ $re ]] || [[ $chromosome == "W" ]] || [[ $chromosome == "Z" ]]
-        then
-                printf "%s\t%s\n" $chromosome $length >> maf/$genome/$genome.txt
-        fi
-done
-
-
-echo "Makes adjacent or sliding windows across a genome or BED file"
-bedtools makewindows -g maf/$genome/$genome.txt -w 2000000 | while read chromosome start end;do printf "%s\t%s\t%s\n" $chromosome $start $end > maf/$genome/$chromosome.$start.$end.bed;done
+halStats --bedSequences $genome $hal | cut -f1,3 | awk '{if($1 < 100 || $1 == "W" || $1 == "Z")print}' | sort -k1,1 -k2,2n > maf/$genome/$genome.txt
 
 
 echo "Convert hal database to maf"
-for bed in maf/$genome/*.bed
+cat maf/$genome/$genome.txt | while read chromosome length
 do
-        bsub -R'select[mem>180000] rusage[mem=180000]' -M180000 -n 15 -q basement -G rdgroup -J hal2maf -o output_%J -e error_%J hal2maf --refGenome $genome \
-        --noAncestors --onlyOrthologs --refTargets $bed $hal maf/$genome/$genome.$(basename $bed .bed).maf
+        bsub -R'select[mem>180000] rusage[mem=180000]' -M180000 -n 15 -q basement -G rdgroup -J hal2maf -o output_%J -e error_%J hal2maf --refGenome $genome --refSequence $chromosome --noAncestors --onlyOrthologs $hal maf/$genome/$genome.$chromosome.maf
 done
+
 
