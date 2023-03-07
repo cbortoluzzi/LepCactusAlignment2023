4425b597de150aefd15ce91d39d12179e288bd6f
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-03-07 10:57:55 +0000

    Update download_hal.sh

--- qc_cactus_alignment/download_hal.sh
+++ qc_cactus_alignment/download_hal.sh
@@ -6,4 +6,6 @@
 
 
 # Download HAL alignment from Ensembl Rapid Release
-https://ftp.ensembl.org/pub/rapid-release/data_files/multi/hal_files/Lepidoptera_88_way-202201.hal
+https://ftp.ensembl.org/pub/rapid-release/data_files/multi/hal_files/Lepidoptera_88_way-202201.hal --directory-prefix=hal
+
+
